3f49a30798d49523ea8e4fed081edcb48895423a
PinkPantser <112284600+AminesSandbox@users.noreply.github.com>
2023-04-03 01:12:22 -0400

    Added collision mechanics for Pawn (Now Pawn can move 1 or 2 squares from initial position and recognises when there is a pawn from the other team or its team in front of it) Still must add Eating for pawn, and also enpassant and remove fact that pawn can walk onto other teams pawn and not eat it.

--- chesspiece.py
+++ chesspiece.py
@@ -4,27 +4,27 @@ import pygame
 class Piece:
     """Piece is a super class of the chess pieces, x and y are the current board coordinates while colour is either black or white, (defined by an integer, 0 or 1)"""
     
-    board = [[0 for i in range(8)] for j in range(8)]
+    board = [[None for i in range(8)] for j in range(8)] #coords go from 0,0 (top left) to 7,7 (bottom right) [y,x]
     
     def __init__(self,x,y,col):
-        self.x = 220 + x * 120
-        self.y = 180 + y * 120
+        self.x = x
+        self.y = y
 
-        Piece.board[self.x][self.y] = self
+        Piece.board[y][x] = self # must do -1 bc indices must be from 0 to 7
         self.col = col
 
     def getCoords(self):
-        #Will return the pixel position of the Piece
-        return (self.x,self.y)
+        """Will return the pixel position of the Piece"""
+        return (220 + self.x * 120, 180 + self.y * 120)
     
     def setCoords(self, x, y):
-        #Update the pixel position of the Piece
-        self.x = 160+ x *180
-        self.y = 160+ y *180
+        """Update the board position of the Piece"""
+        self.x = x
+        self.y = y
 
     def getBoardCoords(self):
-        #Will return the board position of the Piece
-        return (self.x/180-160,self.y/180-160)
+        """Will return the board position of the Piece"""
+        return (self.x, self.y)
     
     def possibleMoves(self):
         return
@@ -32,43 +32,56 @@ class Piece:
     def draw(self,screen,colour):
         pygame.draw.circle(screen,colour,(self.getCoords()),50)
     
-    def collision(self, other)->bool:
-        if not other.isinstance(Piece):
-            raise TypeError("Other is not a Piece")
-        
-        if self.x == other.x and self.y == other.y:
+    def collision(self, increment)->bool:
+        """Checks if there will be a collision with a piece when hcecking a tentative move"""
+        if Piece.board[self.y + increment[1]][self.x + increment[0]] != None:
             return True
         else:
             return False
 
     def getTeam(self):
+        """Returns current Piece's team colour"""
         return self.col
 
 
 class Pawn(Piece):
     def possibleMoves(self):
-        possMove
+        possMove = list()
+        movementincrement = 1 #Pawn can only move one square upwards on all moves but the first
+        if self.y == 1 or self.y == 6:
+            movementincrement = 2
 
-        for i in range(2):
-            if not self.collision:
-                possMove += [0,i]
-            #else: if 
-
-
-        if self.getTeam(): #if on white team
-            if self.y == 2: #if its on the starting position
-                return [[0,1],[0,2]]
-            return [[0,1]]
+        print(movementincrement)
+        if self.getTeam():
+            for i in range(1,movementincrement+1):
+                print(self.collision([0,i]))
+                print(i)
+                if self.collision([0,i]):
+                    print()
+                    if Piece.board[self.y+i][self.x].getTeam() != self.getTeam():
+                        possMove.append([0,i])
+                    return possMove
+                else:
+                    possMove.append([0,i])
         else:
-            if self.y == 7: #if its on the starting position
-                return [[0,-1],[0,-2]]
-            return [[0,-1]]
-    
+            for i in range(1,movementincrement):
+                if self.collision([0,-i]):
+                    if Piece.board[self.y-i][self.x].getTeam() != self.getTeam():
+                        possMove.append([0,-i])
+                    return possMove
+                else:
+                    possMove.append([0,-i])
         
-
-
+        return possMove
+        
 
 
 if __name__ == '__main__':
     print("Im doing something in TestFile")
+    b1 = Pawn(7,1,1)
+    b2 = Pawn(7,3,1)
+    for row in Piece.board:
+        print(row)
+    
+    print(b1.possibleMoves())
 
